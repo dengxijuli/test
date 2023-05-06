ddb5ca858abd2fea1722f2499d3eef778e4e813e
dx <2936322204@qq.com>
2023-05-06 11:41:09 +0800

    test

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @program: test
+ * @description:
+ * @author: dx
+ * @create: 2023/5/6 11:33
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hi world");
+    }
+}
